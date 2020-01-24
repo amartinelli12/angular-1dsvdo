3250dd1ff790fc7d554370aabe6f86385a4236e0
amartinelli12 <ajmartinelli6577@gmail.com>
2020-01-24 11:11:18 -0700

    6 files modified, 1 added

--- src/app/open-close.component.ts
+++ src/app/open-close.component.ts
@@ -1,10 +1,53 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: 'open-close',
+  templateUrl: '../../open-close.component.html',
+  styles: [`:host {
+  display: block;
+}
+
+.open-close-container {
+  border: 1px solid #dddddd;
+  margin-top: 1em;
+  padding: 20px 20px 0px 20px;
+  color: #000000;
+  font-weight: bold;
+  font-size: 20px;
+}`],
+  animations: [
+    trigger('overlayToggled', [
+      state('false', style({ display: 'none', opacity: 0 })),
+      state('true', style({ display: 'block', opacity: 0.8 })),
+      transition('* => *', animate('.2s')),
+    ]),
+    trigger('sideBarToggled', [
+      state('false', style({ display: 'none', width: 0 })),
+      state('true', style({ display: 'block', width: 400 })),
+      transition('* => *', [animate('.3s')]),
+    ]),
+  ],
 })
-export class HelloComponent  {
-  @Input() name: string;
+export class OpenCloseComponent  {
+  isOpen = false;
+  loading = false;
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  openSideNav() {
+    this.isOpen = !this.isOpen;
+    this.loading = false;
+    console.log(this.isOpen);
+  }
+
 }
